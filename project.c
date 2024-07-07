a471d0befe48a5ef74d0fe066587ae88348de379
matt11matthew <matt11matthew@gmail.com>
2024-07-06 21:58:23 -0400

    1

--- project.c
+++ project.c
@@ -16,6 +16,8 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 int c =0;
 
 /* 10 Points */
+
+
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction) {
 
 
@@ -24,29 +26,80 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction) {
         return 1;//WORD NOT ALIGNED
     }
 
-    int arrayIndex = PC >> 2; //Get index
+//    printf("FIRST PC: %d\n", PC);
+//    //16384 is 0x4000
+//    printf("arrayIndex: %d\n", arrayIndex);
+//
+//    int arrayIndex = PC >> 2; //Get index
+//    printf("Decimal memory value: %d\n", Mem[ (PC >> 2) ]);
+    *instruction = Mem[ (PC >> 2) ]; //Load instructions
 
-    *instruction = Mem[arrayIndex]; //Load instructions
-
-
+    //100001
     return 0; //DONT HALT
-//0010000 1000010000000000000000001
+
+
 }
+
 
 /* instruction partition */
 /* 10 Points */
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
+//    printf("INSTRUCTION: %d\n", instruction);
     //111111 = 63
-    *op = (instruction & 0xFC000000) >> 26;
-}
+    int decOpCode  = (0xFC000000&instruction ) >> 26;
+    *op = decOpCode;
 
+    if (decOpCode== 0) {
+        //R-Type Since OP CODE is 0 {
+        int rs =   (0x3E00000&instruction ) >> 21; //Performing bit masking and shifting 21 bits
+        int rt =   (0x1F0000&instruction ) >> 16; //Performing bit masking and shifting 16 bits
+
+//        printf
+
+    } else if (decOpCode == 3 || decOpCode==2){
+        //J-Type
+        *jsec = decOpCode<<6;//UNSURE IF THIS WORKS
+
+
+
+    } else {
+        //REMAINING I-Type
+
+        //CREDIT: https://www.rapidtables.com/convert/number/hex-to-binary.html
+        int rs =   (0x3E00000&instruction ) >> 21; //Performing bit masking and shifting 21 bits
+        int rt =   (0x1F0000&instruction ) >> 16; //Performing bit masking and shifting 16 bits
+        int immediate =   (0xFFFF&instruction ); //Performing bit masking s
+
+        *r1 = rs;
+        *r2 = rt;
+        *offset = immediate;
+
+//        printf("RS: %d\n", rs);
+//        printf("RT: %d\n", rt);
+//        printf("Immediate: %d\n", immediate);
+//
+//        if (decOpCode==35||decOpCode==43||decOpCode==4||decOpCode==5) {
+//            //OFFSET
+//            *offset = immediate;
+//        } else {
+//            // immediate
+//        }
+    }
+
+
+
+
+}
 
 
 /* instruction decode */
 /* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
+
+    return 1;
+
 
 }
 
