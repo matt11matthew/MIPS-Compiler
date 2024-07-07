e65df3f8d0775024d11a13b3131dd9ede86f5026
matt11matthew <matt11matthew@gmail.com>
2024-07-06 20:38:11 -0400

    1
    1

---
+++ project_final.c
@@ -0,0 +1,96 @@
+#include "spimcore.h"
+
+
+/* ALU */
+/* 10 Points */
+void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
+{
+
+}
+
+/* instruction fetch */
+// HEX 21080001
+// 001000 01000010000000000000000001
+
+
+int c =0;
+
+/* 10 Points */
+int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction) {
+
+
+
+    if (PC % 2 != 0) {
+        return 1;//WORD NOT ALIGNED
+    }
+
+    int arrayIndex = PC >> 2; //Get index
+
+    *instruction = Mem[arrayIndex]; //Load instructions
+
+  
+    return 0; //DONT HALT
+//0010000 1000010000000000000000001
+}
+
+/* instruction partition */
+/* 10 Points */
+void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
+{
+    //111111 = 63
+    op *= (instruction & 0xFC000000) >> 26;
+}
+
+
+
+/* instruction decode */
+/* 15 Points */
+int instruction_decode(unsigned op,struct_controls *controls)
+{
+
+}
+
+/* Read Register */
+/* 5 Points */
+void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
+{
+
+}
+
+
+/* Sign Extend */
+/* 10 Points */
+void sign_extend(unsigned offset,unsigned *extended_value)
+{
+
+}
+
+/* ALU operations */
+/* 10 Points */
+int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
+{
+
+}
+
+/* Read / Write Memory */
+/* 10 Points */
+int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
+{
+
+}
+
+
+/* Write Register */
+/* 10 Points */
+void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
+{
+
+}
+
+/* PC update */
+/* 10 Points */
+void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
+{
+
+}
+
